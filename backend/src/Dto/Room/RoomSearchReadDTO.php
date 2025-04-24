40c7cc949915de7e2da7636efb9c4447ff56ac29
Martin <martinsimongo@gmail.com>
2025-04-24 14:41:05 +0200

    feat: Add Room Search functionality with DTOs and processor for filtering rooms based on criteria

---
+++ backend/src/Dto/Room/RoomSearchReadDTO.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Dto\Room;
+
+use Symfony\Component\Serializer\Annotation\Groups;
+use App\Dto\Hotel\HotelReadDTO;
+
+final class RoomSearchReadDTO
+{
+    #[Groups(['readSearch:room'])]
+    public ?int $id = null;
+
+    #[Groups(['readSearch:room'])]
+    public ?string $name = null;
+
+    #[Groups(['readSearch:room'])]
+    public ?string $description = null;
+
+    #[Groups(['readSearch:room'])]
+    public ?float $price = null;
+
+    #[Groups(['readSearch:room'])]
+    public ?string $folderImage = null;
+
+    #[Groups(['readSearch:room'])]
+    public ?int $capacity = null;
+
+    /**
+     * @var FeatureReadDTO[]
+     */
+    #[Groups(['readSearch:room'])]
+    public array $features = [];
+
+    /**
+     * @var HotelReadDTO
+     */
+    #[Groups(['readSearch:room'])]
+    public ?HotelReadDTO $hotel = null;
+}
