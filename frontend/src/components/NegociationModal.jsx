062025f367ee4e9b8cb1abe7fe15671309ddd46f
Jérémy Gross <jeremygross@MacBook-Pro-de-Jeremy.local>
2025-04-24 10:14:00 +0200

    modal de negociation

---
+++ frontend/src/components/NegociationModal.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Modal, Box } from '@mui/material';
+
+const NegociationModal = ({ isOpen, onClose, data }) => {
+  return (
+    <Modal 
+      open={isOpen} 
+      onClose={onClose} 
+      sx= {{
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        backdropFilter: 'blur(5px)',
+      }}
+    >
+      <Box
+        className="absolute top-1/2 left-1/2 bg-white rounded-lg shadow-xl flex p-6 gap-6"
+        style={{
+          transform: 'translate(-50%, -50%)',
+          width: '50%',
+          maxHeight: '50%',
+          overflowY: 'auto'
+        }}
+      >
+        <div className="relative w-1/2 rounded-xl overflow-hidden">
+          <img
+            src={data.imageUrl}
+            alt={data.name}
+            className="w-full h-full object-cover"
+          />
+          <div className="absolute bottom-0 left-0 w-full bg-black/75 text-white p-4">
+            <h3 className="font-bold text-lg">{data.name}</h3>
+            <p className="text-sm">Prix initial : {data.price} € / nuits</p>
+          </div>
+        </div>
+
+        <div className="w-1/2 flex flex-col justify-center gap-4">
+          <input
+            type="number"
+            style={{
+              boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+            }}
+            placeholder="Entrez votre proposition de prix..."
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-main"
+            required
+          />
+          <div className="flex flex-row gap-4">
+            <button
+              onClick={onClose}
+              className="w-4/12 py-3 bg-blue-main text-white font-semibold rounded-lg shadow-md hover:bg-blue-900 transition"
+              
+            >
+              Annuler
+            </button>
+            <input
+              type="submit"
+              className="w-full py-3 bg-blue-main text-white font-semibold rounded-lg shadow-md hover:bg-blue-900 transition"
+              value="Envoyer"
+            />
+          </div>
+        </div>
+      </Box>
+    </Modal>
+  );
+};
+
+export default NegociationModal;
