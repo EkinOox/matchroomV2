90e02de896a0ce90c4ada26df005f687e769b903
Yoseinoo <skycream@outlook.fr>
2025-04-24 11:17:03 +0200

    Negotiations page

--- frontend/src/pages/Negotiations.jsx
+++ frontend/src/pages/Negotiations.jsx
@@ -1,17 +1,107 @@
 import Navbar from '../components/Navbar';
 import { useEffect, useState } from 'react';
-import Tabs from '../components/Tabs';
-import HotelCard from '../components/HotelCard';
+import Tabs from '../components/NegotiationTabs';
 import { fetchHotels } from '../api/hotels';
+import NegotiationCard from '../components/NegotiationCard';
+import NegotiationTabs from '../components/NegotiationTabs';
 
 const Negotiations = () => {
   const [status, setStatus] = useState('pending');
-  const [hotels, setHotels] = useState([]);
+  const [negociations, setNegociations] = useState([]);
+
+  const negociationSet = [
+    {
+      id: 1,
+      status: 'pending',
+      proposed_price: 120,
+      counter_offer: 0,
+      room: {
+        name: "Room 1",
+        price: 150,
+        folder_image: "",
+        capacity: 2
+      }
+    },
+    {
+      id: 2,
+      status: 'pending',
+      proposed_price: 100,
+      counter_offer: 0,
+      room: {
+        name: "Room 2",
+        price: 200,
+        folder_image: "",
+        capacity: 1
+      }
+    },
+    {
+      id: 3,
+      status: 'approved',
+      proposed_price: 95,
+      counter_offer: 0,
+      room: {
+        name: "Room 3",
+        price: 100,
+        folder_image: "",
+        capacity: 4
+      }
+    },
+    {
+      id: 4,
+      status: 'approved',
+      proposed_price: 180,
+      counter_offer: 0,
+      room: {
+        name: "Room 4",
+        price: 195,
+        folder_image: "",
+        capacity: 1
+      }
+    },
+    {
+      id: 5,
+      status: 'rejected',
+      proposed_price: 45,
+      counter_offer: 0,
+      room: {
+        name: "Room 5",
+        price: 100,
+        folder_image: "",
+        capacity: 1
+      }
+    },
+    {
+      id: 6,
+      status: 'rejected',
+      proposed_price: 80,
+      counter_offer: 0,
+      room: {
+        name: "Room 6",
+        price: 100,
+        folder_image: "",
+        capacity: 2
+      }
+    },
+    {
+      id: 7,
+      status: 'challenged',
+      proposed_price: 100,
+      counter_offer: 150,
+      room: {
+        name: "Room 7",
+        price: 200,
+        folder_image: "",
+        capacity: 3
+      }
+    },
+  ]
 
   useEffect(() => {
-    fetchHotels(status)
+    /*fetchHotels(status)
       .then(setHotels)
       .catch(console.error);
+    */
+    setNegociations(negociationSet.filter(nego => nego.status === status));
   }, [status]);
 
   return (
@@ -20,23 +110,20 @@ const Negotiations = () => {
     <div className="min-h-screen bg-gray-100 flex justify-center items-start py-12 px-4">
       <div className="w-full max-w-4xl bg-white rounded-2xl shadow-md p-6">
         {/* En-tête */}
-        <div className="flex justify-between items-center mb-6">
+        <div className="flex justify-center items-center mb-6">
           <h1 className="text-2xl font-bold text-gray-800">Négociations</h1>
-          <span className="text-sm text-gray-500">Menu</span>
         </div>
 
         {/* Onglets */}
-        <Tabs active={status} onChange={setStatus} />
+        <NegotiationTabs active={status} onChange={setStatus} />
 
         {/* Liste des hôtels */}
         <div className="mt-6 space-y-6">
-          {hotels.length > 0 ? (
-            hotels.map((hotel) => (
-              <HotelCard
-                key={hotel.id}
-                name={hotel.name}
-                price={hotel.price}
-                imageUrl={hotel.imageUrl}
+          {negociations.length > 0 ? (
+            negociations.map((nego) => (
+              <NegotiationCard
+                key={nego.id}
+                negotiation={nego}
               />
             ))
           ) : (
