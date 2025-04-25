3addeaa9750d7c6ca4ded924fc861e555738dfc9
Kyllian Diochon <77027134+EkinOox@users.noreply.github.com>
2025-04-25 22:55:25 +0200

    Update responsive login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -40,7 +40,7 @@ export default function Login({ onLoginSuccess }) {
   return (
     <div className="w-full relative h-screen overflow-hidden flex flex-col justify-center items-center">
       <AnimatedGridPattern />
-      <div className="w-4/12 p-8 bg-white z-50 border rounded-lg shadow">
+      <div className="w-4/5 md:w-4/12 p-8 bg-white z-50 border rounded-lg shadow">
         <h2 className="text-4xl font-bold mb-6">Connexion</h2>
         <span className="font-bold tracking-tight">Veuillez vous authentifier afin d'avoir accès à la recherche d'hotels</span>
         <form onSubmit={handleLogin} className="flex flex-col mt-6 gap-6">
